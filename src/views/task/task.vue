37ed51cd68d64301567afb034efe864858c34d62
sunlong <sunlong@work.com>
2022-01-12 17:17:53 +0800

    feat(uncomplete): push task to remote cause losing power...

--- src/views/task/task.vue
+++ src/views/task/task.vue
@@ -1,13 +1,14 @@
 <script lang="ts" setup>
 import { onBeforeMount, ref, Ref, onMounted } from 'vue'
 import { ElMessage } from 'element-plus'
+import { Link } from '@element-plus/icons-vue'
 import { BindedGithubRepo, DotpayTask } from '@/entity'
 import { get_binded_repos, get_tasks } from '@/api/user'
 import Router from '@/router'
 // =============== Datas ===============
 const binded_repos: Ref<BindedGithubRepo[]> = ref([])
 const activeTabIndex = ref('0')
-const activeRepoIndex = ref(1)
+const activeAuthorRepoIndex = ref(1)
 const taskStatus = ref('')
 const statusOptions = ref([
   { label: 'Created', value: 'created' },
@@ -15,19 +16,28 @@ const statusOptions = ref([
   { label: 'Completed', value: 'completed' },
 ])
 const authorTasks: Ref<DotpayTask[]> = ref([])
+const authorTasksForTabel: Ref<DotpayTask[]> = ref([])
 // =============== Functions ===============
 function gotoBindPage() {
   Router.push({ name: 'bind' })
 }
+function gotoTaskUrl(url: string) {
+  window.open(url)
+}
 const handleClick = (tab: string, event: Event) => {
   console.log(activeTabIndex.value)
 }
-async function onRepoChanged() {
-  if (typeof activeRepoIndex.value !== 'number') {
+async function onAuthorRepoChanged() {
+  if (typeof activeAuthorRepoIndex.value !== 'number') {
     return
   }
-  const selectedRepo = binded_repos.value[activeRepoIndex.value]
+  const selectedRepo = binded_repos.value[activeAuthorRepoIndex.value]
   authorTasks.value = await get_tasks(selectedRepo.repo_id)
+  authorTasksForTabel.value = authorTasks.value
+}
+function onStatusChange(val: any) {
+  if (!val) return authorTasksForTabel.value = authorTasks.value
+  authorTasksForTabel.value = authorTasks.value.filter(i => i.status === val)
 }
 // =============== Hooks ===============
 onBeforeMount(async () => {
@@ -40,17 +50,16 @@ onBeforeMount(async () => {
     gotoBindPage()
   }
   binded_repos.value = r
-  await onRepoChanged()
+  await onAuthorRepoChanged()
 })
 onMounted(async () => {
   console.log('mounted')
-
 })
 </script>
 
 <template>
   <el-row style="margin-top: 20px">
-    <el-col :span="8">
+    <el-col :span="6">
       <div class="side-bar">
         <div class="line-one">
           <h2 class="text-brief">Repos</h2>
@@ -72,7 +81,7 @@ onMounted(async () => {
         >
           <!-- Author -->
           <el-tab-pane label="My Repo" class="repo-list">
-            <el-collapse v-model="activeRepoIndex" accordion @change="onRepoChanged()">
+            <el-collapse v-model="activeAuthorRepoIndex" accordion @change="onAuthorRepoChanged()">
               <el-collapse-item
                 v-for="(item, index) in binded_repos"
                 :title="item.repo_name"
@@ -83,12 +92,14 @@ onMounted(async () => {
             </el-collapse>
           </el-tab-pane>
           <!-- Developer -->
-          <el-tab-pane label="My Task">My Task</el-tab-pane>
+          <el-tab-pane label="My Task" class="dev-pane">
+            <div>All Your Task Would List In Right Table</div>
+          </el-tab-pane>
         </el-tabs>
       </div>
     </el-col>
     <!-- Content Area -->
-    <el-col :span="16">
+    <el-col :span="18">
       <div class="main-card-author" v-if="activeTabIndex === '0'">
         <div class="menu-bar">
           <div style="font-weight: 600;">Tasks</div>
@@ -98,6 +109,8 @@ onMounted(async () => {
             placeholder="All"
             size="small"
             style="width: 150px;margin-left: 15px;"
+            clearable
+            @change="onStatusChange"
           >
             <el-option
               v-for="item in statusOptions"
@@ -108,16 +121,69 @@ onMounted(async () => {
           </el-select>
         </div>
         <div class="task-table">
-          <el-table :data="authorTasks" stripe style="width: 100%">
-            <el-table-column prop="id" label="id" width="180" />
-            <el-table-column prop="pay" label="Price" width="180" />
-            <el-table-column prop="title" label="Title" />
-            <el-table-column prop="describe" label="Description" />
-            <el-table-column prop="task_url" label="Link" />
+          <el-table :data="authorTasksForTabel" stripe style="width: 100%">
+            <el-table-column label="id" width="120">
+              <template #default="scope">#{{ scope.row.issue_id }}</template>
+            </el-table-column>
+            <el-table-column label="Price" width="140">
+              <template #default="scope">{{ scope.row.pay }} DOT</template>
+            </el-table-column>
+            <el-table-column prop="title" label="Title" width="140" />
+            <el-table-column prop="describe" label="Description" width="140" />
+            <el-table-column prop="status" label="Status" />
+            <el-table-column prop="developer.login" label="Developer" width="120" />
+            <el-table-column prop="task_url" label="Link">
+              <template #default="scope">
+                <el-icon size="25" color="#6667AB" @click="gotoTaskUrl(scope.row.task_url)">
+                  <Link />
+                </el-icon>
+              </template>
+            </el-table-column>
           </el-table>
         </div>
       </div>
-      <div class="main-card-dev" v-else></div>
+      <div class="main-card-dev" v-else>
+        <div class="menu-bar">
+          <div style="font-weight: 600;">Tasks</div>
+          <div style="margin-left: 20px;color:darkgray">Status</div>
+          <el-select
+            v-model="taskStatus"
+            placeholder="All"
+            size="small"
+            style="width: 150px;margin-left: 15px;"
+            clearable
+            @change="onStatusChange"
+          >
+            <el-option
+              v-for="item in statusOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+        <div class="task-table">
+          <el-table :data="authorTasksForTabel" stripe style="width: 100%">
+            <el-table-column label="id" width="120">
+              <template #default="scope">#{{ scope.row.issue_id }}</template>
+            </el-table-column>
+            <el-table-column label="Price" width="140">
+              <template #default="scope">{{ scope.row.pay }} DOT</template>
+            </el-table-column>
+            <el-table-column prop="title" label="Title" width="140" />
+            <el-table-column prop="describe" label="Description" width="140" />
+            <el-table-column prop="status" label="Status" />
+            <el-table-column prop="developer.login" label="Developer" width="120" />
+            <el-table-column prop="task_url" label="Link">
+              <template #default="scope">
+                <el-icon size="25" color="#6667AB" @click="gotoTaskUrl(scope.row.task_url)">
+                  <Link />
+                </el-icon>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+      </div>
     </el-col>
   </el-row>
 </template>
@@ -139,6 +205,10 @@ onMounted(async () => {
       height: 30px;
     }
   }
+  .dev-pane {
+    @include flex-row-center;
+    text-align: center;
+  }
 }
 .main-card-author {
   margin: 85px 0 0 25px;
@@ -152,6 +222,14 @@ onMounted(async () => {
   }
 }
 .main-card-dev {
-  margin-left: 20px;
+  margin: 85px 0 0 25px;
+  .menu-bar {
+    display: flex;
+    align-items: flex-end;
+    width: 100%;
+  }
+  .task-table {
+    margin-top: 20px;
+  }
 }
 </style>
