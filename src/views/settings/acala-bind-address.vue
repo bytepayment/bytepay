c878074c36fc6eab67be7760e420fd4db3b19623
丁延康 <1120252308@qq.com>
2022-05-12 21:33:23 +0800

    setting

--- src/views/settings/acala-bind-address.vue
+++ src/views/settings/acala-bind-address.vue
@@ -31,7 +31,7 @@ onMounted(async () => {
       <span>
         <strong>Notice: Just For Developer</strong>
       </span>
-      <p>When you authorized your github account to Crypto Pay Lab, we will generate an Polka Account by default, you can withdraw the dot you eraned anytime.</p>
+      <p>When you authorized your github account to Crypto Pay Lab, we will generate an Acala Account by default, you can withdraw the dot you eraned anytime.</p>
       <p>Also We support bind your own account here, after you finished task, author will pay into your own account directly.</p>
     </div>
     <el-divider />
