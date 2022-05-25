edc9edd2cc5fb9d60679f60d23cb07b6845059ad
DNN <1524757775@qq.com>
2022-05-25 16:48:16 +0800

    交易记录 移除嵌套卡片

--- src/views/properties/components/TransactionRecords.vue
+++ src/views/properties/components/TransactionRecords.vue
@@ -20,44 +20,42 @@ function addressFormat(address: string) {
 </script>
 
 <template>
-<el-card class="txs">
-    <el-table :data="list" height="450" stripe style="width: 100%">
-        <el-table-column label="Extrinsic Index" prop="extrinsic_index" width="150" />
-        <el-table-column label="Block" prop="block_num" width="120" />
-        <el-table-column label="Time" prop="block_timestamp" width="140">
-            <template #default="scope">{{ moment.unix(scope.row.block_timestamp).fromNow() }}</template>
-        </el-table-column>
-        <el-table-column label="From">
-            <template #default="scope">
-                <a v-if="scope.row.from !== address"
-                   :href="`${baseUrl}/account/${scope.row.from}`"
-                   target="_blank">{{ addressFormat(scope.row.from) }}
-                </a>
-                <span v-else>{{ addressFormat(scope.row.from) }}</span>
-            </template>
-        </el-table-column>
-        <el-table-column label="To">
-            <template #default="scope">
-                <a v-if="scope.row.to !== address" :href="`${baseUrl}/account/${scope.row.to}`" target="_blank">{{ addressFormat(scope.row.to) }}</a>
-                <span v-else>{{ addressFormat(scope.row.to) }}</span>
-            </template>
-        </el-table-column>
-        <el-table-column label="Status" width="100">
-            <template #default="scope">
-                <el-tag v-if="scope.row.success" type="success">Success</el-tag>
-                <el-tag v-else type="danger">Failed</el-tag>
-            </template>
-        </el-table-column>
-        <el-table-column label="Amount" width="120">
-            <template #default="scope">{{ scope.row.amount }} {{ platform }}</template>
-        </el-table-column>
-        <el-table-column label="Hash">
-            <template #default="scope">
-                <a :href="`${baseUrl}/extrinsic/${scope.row.hash}`" target="_blank">{{ addressFormat(scope.row.hash) }}</a>
-            </template>
-        </el-table-column>
-    </el-table>
-</el-card>
+<el-table :data="list" class="txs" height="450" stripe style="width: 100%">
+    <el-table-column label="Extrinsic Index" prop="extrinsic_index" width="150" />
+    <el-table-column label="Block" prop="block_num" width="120" />
+    <el-table-column label="Time" prop="block_timestamp" width="140">
+        <template #default="scope">{{ moment.unix(scope.row.block_timestamp).fromNow() }}</template>
+    </el-table-column>
+    <el-table-column label="From">
+        <template #default="scope">
+            <a v-if="scope.row.from !== address"
+               :href="`${baseUrl}/account/${scope.row.from}`"
+               target="_blank">{{ addressFormat(scope.row.from) }}
+            </a>
+            <span v-else>{{ addressFormat(scope.row.from) }}</span>
+        </template>
+    </el-table-column>
+    <el-table-column label="To">
+        <template #default="scope">
+            <a v-if="scope.row.to !== address" :href="`${baseUrl}/account/${scope.row.to}`" target="_blank">{{ addressFormat(scope.row.to) }}</a>
+            <span v-else>{{ addressFormat(scope.row.to) }}</span>
+        </template>
+    </el-table-column>
+    <el-table-column label="Status" width="100">
+        <template #default="scope">
+            <el-tag v-if="scope.row.success" type="success">Success</el-tag>
+            <el-tag v-else type="danger">Failed</el-tag>
+        </template>
+    </el-table-column>
+    <el-table-column label="Amount" width="120">
+        <template #default="scope">{{ scope.row.amount }} {{ platform }}</template>
+    </el-table-column>
+    <el-table-column label="Hash">
+        <template #default="scope">
+            <a :href="`${baseUrl}/extrinsic/${scope.row.hash}`" target="_blank">{{ addressFormat(scope.row.hash) }}</a>
+        </template>
+    </el-table-column>
+</el-table>
 </template>
 
 <style scoped>
