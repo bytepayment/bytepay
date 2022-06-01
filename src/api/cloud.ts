5a3c3de3f860e26a262bacd785cdedd482d21cbb
WangZiJun <595563214@qq.com>
2022-06-01 16:31:10 +0800

    fix:增加token

--- src/api/cloud.ts
+++ src/api/cloud.ts
@@ -1,11 +1,15 @@
-import { Cloud } from 'laf-client-sdk';
-import { getToken } from '../utils/auth';
+import { Cloud } from "laf-client-sdk";
 
 // const API_BASE_URL = 'https://6119a1ac-1e79-4449-8665-f4f7d3066a5a.lafyun.com'
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+function getToken() {
+  const token = JSON.parse(localStorage.getItem("user") as string)?.token;
+  return token;
+}
 
 export const cloud = new Cloud({
   baseUrl: API_BASE_URL,
-  dbProxyUrl: '/proxy/console',
-  getAccessToken: getToken as any
-})
+  dbProxyUrl: "/proxy/console",
+  getAccessToken: getToken as any,
+});
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,40 +1,40 @@
-import { GithubUser } from '@/entity'
+import { GithubUser } from "@/entity";
 
-const kToken = 'access_token'
-const kExpire = 'token_expire'
-const kUser = 'user'
+const kToken = "access_token";
+const kExpire = "token_expire";
+const kUser = "user";
 
 export function getToken() {
-  const token = localStorage.getItem(kToken)
-  const expire = getTokenExpire()
+  const token = localStorage.getItem(kToken);
+  const expire = getTokenExpire();
 
   if (!expire || expire <= Date.now() / 1000) {
-    removeToken()
+    removeToken();
   }
-  return token
+  return token;
 }
 
 export function getTokenExpire() {
-  const expire = parseInt((localStorage.getItem(kExpire) as any) || 0)
-  return expire
+  const expire = parseInt((localStorage.getItem(kExpire) as any) || 0);
+  return expire;
 }
 
 // default expires at 2099-12-31 23:59:59
-export function setToken(token: string, expire: string = '4102415999000') {
-  localStorage.setItem(kExpire, expire)
-  return localStorage.setItem(kToken, token)
+export function setToken(token: string, expire: string = "4102415999000") {
+  localStorage.setItem(kExpire, expire);
+  return localStorage.setItem(kToken, token);
 }
 
 export function removeToken() {
-  localStorage.removeItem(kExpire)
-  localStorage.removeItem(kUser)
-  return localStorage.removeItem(kToken)
+  localStorage.removeItem(kExpire);
+  localStorage.removeItem(kUser);
+  return localStorage.removeItem(kToken);
 }
 
 export function getUser(): GithubUser {
-  return JSON.parse(localStorage.getItem(kUser) as any)
+  return JSON.parse(localStorage.getItem(kUser) as any);
 }
 
 export function setUser(user: any) {
-  return localStorage.setItem(kUser, user)
+  return localStorage.setItem(kUser, user);
 }
